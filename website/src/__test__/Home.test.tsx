ca8e63cbac975f8f3c39fd310d05dff416c5f809
jkellerer <jkellerer@posteo.de>
2024-01-11 08:28:54 +0100

    feat: use next 14 with yarn

--- website/src/__test__/Home.test.tsx
+++
@@ -1,8 +0,0 @@
-import { expect, test } from "vitest";
-import { render, screen, within } from "@testing-library/react";
-import Home from "../pages/index";
-
-test("home", () => {
-  render(<Home />);
-  expect(screen.getByText("Some Text")).toBeDefined();
-});
