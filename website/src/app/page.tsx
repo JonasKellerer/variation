3efa32ac53f7311332d533e2d8402c3e7e4366ee
jkellerer <jkellerer@posteo.de>
2024-01-29 09:02:58 +0100

    feat: add vitest for unit tests

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-            <div>Variation study</div>
+            <h1>Variation study</h1>
         </main>
     );
 }
