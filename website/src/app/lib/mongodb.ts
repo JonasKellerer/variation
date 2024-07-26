092c87bffee596d615bb3d57bbdc0ce0288f3ca9
jkellerer <jkellerer@posteo.de>
2024-07-26 17:43:29 +0200

    feat: use global variable for db client in dev modus

--- website/src/app/lib/mongodb.ts
+++ website/src/app/lib/mongodb.ts
@@ -1,16 +1,43 @@
-import { MongoClient } from 'mongodb';
+import { Db, MongoClient } from 'mongodb';
 
 const uri = process.env.MONGODB_URI;
 
-if (uri === undefined) {
+if (!uri) {
     throw new Error('Please add your Mongo URI to .env.local');
 }
 
-const client = new MongoClient(uri);
-const clientPromise = client.connect();
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
+declare global {
+    // eslint-disable-next-line no-var
+    var _mongoClientPromise: Promise<MongoClient> | undefined;
+}
+
+if (process.env.NODE_ENV === 'development') {
+    if (!global._mongoClientPromise) {
+        client = new MongoClient(uri);
+        global._mongoClientPromise = client.connect();
+    }
+    clientPromise = global._mongoClientPromise;
+} else {
+    client = new MongoClient(uri);
+    clientPromise = client.connect();
+}
 
 const databaseName = 'variationDatabase';
 export const testCollection = 'testCollection';
-export const connectToDatabase = async () => {
-    return (await clientPromise).db(databaseName);
+
+export const connectToDatabase = async (): Promise<Db> => {
+    if (process.env.NODE_ENV === 'production' && !clientPromise) {
+        client = new MongoClient(uri);
+        clientPromise = client.connect();
+    }
+
+    try {
+        return (await clientPromise).db(databaseName);
+    } catch (error) {
+        console.error('Failed to connect to the database', error);
+        throw new Error('Failed to connect to the database');
+    }
 };
